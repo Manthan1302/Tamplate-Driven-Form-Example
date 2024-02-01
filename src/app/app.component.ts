f1f263c9bdb79f1e5af1aa4ec7aa6e705d3f7fd6
Manthan Thakkar <Manthant@btl.com>
2024-02-01 19:09:07 +0530

    Template Driven Form Example Of Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'formExample';
+  @ViewChild('fom') signupForm!: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  gender = ['male', 'female'];
+  user={
+    name:'',
+    email:'',
+    userName:'',
+    questionAnswer:'',
+    answer:'',
+    gender:''
+  }
+  submitted=false
+  suggestUserName(name:string) {
+    const suggestUserName =name + Math.floor(Math.random()*10000 )
+    // this.signupForm.setValue({
+    //   userData:{
+    //     username:suggestUserName,
+    //     email:''
+    //   },
+    // select:'pet',
+    // questionAnswer:'',
+    // gender:''
+    // })
+
+    this.signupForm.form.patchValue({
+      userData:{
+        username:suggestUserName
+      }
+    });
+  }
+
+  // onSubmit(fom:NgForm){
+  //   console.log(fom);
+
+  // }
+
+  onSubmit() {
+    console.log(this.signupForm);
+    
+    this.submitted=true
+    this.user.userName=this.signupForm.value.userData.username;
+    this.user.name=this.signupForm.value.userData.name;
+    this.user.email=this.signupForm.value.userData.email;
+    this.user.questionAnswer=this.signupForm.value.select;
+    this.user.gender=this.signupForm.value.gender;
+    this.user.answer=this.signupForm.value.questionAnswer;
+
+
+    this.signupForm.reset();
+  }
 }
